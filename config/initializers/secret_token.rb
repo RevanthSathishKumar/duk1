ecc8a5924b2ab7cc364589ebf76f9f567085ce8e
Sathishkumar K <ksathishk9@gmail.com>
2014-11-30 08:07:49 +0530

    Generated a rails application and scaffold for status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Duk1::Application.config.secret_token = '110b53434911fca6887efb1ccfb4107f3c804957484a8e6beb040056e0a1759d9f6f2ed651a8bcc2c04a284dc46654409fdd2d02cccbf7e722c3f29afa2f720d'
